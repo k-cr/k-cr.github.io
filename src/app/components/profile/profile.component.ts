1fa3cf1cae816fc0716e1e062ddb3f2a9e110edb
Cristian Ramos <ramoscristian2704@gmail.com>
2022-03-10 23:55:35 -0300

    Mod: nav, home, skills, projects.

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -16,5 +16,4 @@ export class ProfileComponent implements OnInit {
       this.myPortfolio = data;
     });
   }
-
 }
---
+++ src/assets/js/main.js
@@ -0,0 +1,4 @@
+window.addEventlistener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
